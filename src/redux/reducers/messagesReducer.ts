cc7b7cbc216e8d0d90534f0dfc1922938edae842
Olga Shuliak <wirasta@gmail.com>
2022-07-12 17:18:50 +0300

    add: 41  switch

--- src/redux/reducers/messagesReducer.ts
+++ src/redux/reducers/messagesReducer.ts
@@ -1,20 +1,33 @@
-import {AllActions, MessagePageType, MessageType} from '../types';
+import {AllActions, MessagePageType} from '../types';
 
 
 const UPDATE_NEW_MESSAGE_BODY = 'UPDATE-NEW-MESSAGE-BODY'
 const SEND_MESSAGE = 'SEND-MESSAGE'
 
 const messagesReducer = (state:MessagePageType, action: AllActions) => {
-  if (action.type === UPDATE_NEW_MESSAGE_BODY) {
-    state.newMessageBody = action.body;
-
-  } else if (action.type === SEND_MESSAGE) {
-    let body = state.newMessageBody;
-    state.newMessageBody = '';
-    state.messagesData.push({id: 7, message: body})
+  // if (action.type === UPDATE_NEW_MESSAGE_BODY) {
+  //   state.newMessageBody = action.body;
+  //
+  // } else if (action.type === SEND_MESSAGE) {
+  //   let body = state.newMessageBody;
+  //   state.newMessageBody = '';
+  //   state.messagesData.push({id: 7, message: body})
+  // }
+  //
+  // return state;
+  switch (action.type) {
+    case UPDATE_NEW_MESSAGE_BODY:
+      state.newMessageBody = action.body;
+      return state;
+    case SEND_MESSAGE:
+      let body = state.newMessageBody;
+      state.newMessageBody = '';
+      state.messagesData.push({id: 7, message: body});
+      return state;
+    default:
+      return state;
   }
 
-  return state;
 }
 
 
